8fc2bec2bb49df82008c4080b09c546e150b5d85
Janos Dobronszki <dobronszki@gmail.com>
2020-02-17 16:39:21 +0100

    Test trigger

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"github.com/micro/go-micro/util/log"
 	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/util/log"
 	"github.com/micro/services/helloworld/handler"
 
 	helloworld "github.com/micro/services/helloworld/proto/helloworld"
 )
 
 func main() {
-	// New Service
 	service := micro.NewService(
 		micro.Name("go.micro.srv.helloworld"),
 		micro.Version("latest"),
